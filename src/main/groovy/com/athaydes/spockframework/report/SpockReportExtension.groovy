962235ac5630aca6bdaf6f648b7410f855b81282
Renato Athaydes <renatoathaydes@hotmail.com>
2015-03-01 15:32:55 +0100

    Modified ConfigLoader to load configs correctly if the type of the IReportCreator changes during runtime

--- src/main/groovy/com/athaydes/spockframework/report/SpockReportExtension.groovy
+++ src/main/groovy/com/athaydes/spockframework/report/SpockReportExtension.groovy
@@ -1,9 +1,10 @@
 package com.athaydes.spockframework.report
 
+import com.athaydes.spockframework.report.internal.ConfigLoader
+import com.athaydes.spockframework.report.internal.FeatureRun
+import com.athaydes.spockframework.report.internal.SpecData
+import com.athaydes.spockframework.report.internal.SpecProblem
 import groovy.util.logging.Log
-
-import java.util.logging.Level
-
 import org.spockframework.runtime.IRunListener
 import org.spockframework.runtime.extension.IGlobalExtension
 import org.spockframework.runtime.model.ErrorInfo
@@ -11,10 +12,7 @@ import org.spockframework.runtime.model.FeatureInfo
 import org.spockframework.runtime.model.IterationInfo
 import org.spockframework.runtime.model.SpecInfo
 
-import com.athaydes.spockframework.report.internal.ConfigLoader
-import com.athaydes.spockframework.report.internal.FeatureRun
-import com.athaydes.spockframework.report.internal.SpecData
-import com.athaydes.spockframework.report.internal.SpecProblem
+import java.util.logging.Level
 
 /**
  *
@@ -23,133 +21,131 @@ import com.athaydes.spockframework.report.internal.SpecProblem
 @Log
 class SpockReportExtension implements IGlobalExtension {
 
-	static final PROJECT_URL = 'https://github.com/renatoathaydes/spock-reports'
+    static final PROJECT_URL = 'https://github.com/renatoathaydes/spock-reports'
 
-	def configLoader = new ConfigLoader()
-	String reportCreatorClassName
-	final reportCreatorSettings = [ : ]
-	String outputDir
-	boolean hideEmptyBlocks = false
+    def configLoader = new ConfigLoader()
+    Properties config
+    String reportCreatorClassName
+    String outputDir
+    boolean hideEmptyBlocks = false
 
-	def firstVisit = true
+    @Override
+    void visitSpec( SpecInfo specInfo ) {
+        if ( config == null ) {
+            config()
+        }
+        if ( reportCreatorClassName )
+            try {
+                def reportCreator = instantiateReportCreator()
+                configReportCreator( reportCreator )
+                specInfo.addListener new SpecInfoListener( reportCreator )
+            } catch ( e ) {
+                log.log( Level.FINE, "Failed to create instance of $reportCreatorClassName", e )
+            }
+    }
 
-	@Override
-	void visitSpec( SpecInfo specInfo ) {
-		if ( firstVisit ) {
-			config()
-			firstVisit = false
-		}
-		if ( reportCreatorClassName )
-			try {
-				def reportCreator = instantiateReportCreator()
-				configReportCreator( reportCreator )
-				specInfo.addListener new SpecInfoListener( reportCreator )
+    void config() {
+        log.finer "Configuring ${this.class.name}"
+        config = configLoader.loadConfig()
+        reportCreatorClassName = config.getProperty( IReportCreator.class.name )
+        outputDir = config.getProperty( ConfigLoader.PROP_OUTPUT_DIR )
+        hideEmptyBlocks = Boolean.parseBoolean(
+                config.getProperty( ConfigLoader.PROP_HIDE_EMPTY_BLOCKS )
+        )
+    }
 
-			} catch ( e ) {
-				log.log(Level.FINE, "Failed to create instance of $reportCreatorClassName", e)
-			}
-	}
+    def instantiateReportCreator() {
+        def reportCreatorClass = Class.forName( reportCreatorClassName )
+        reportCreatorClass.asSubclass( IReportCreator ).newInstance()
+    }
 
-	void config() {
-		log.finer "Configuring ${this.class.name}"
-		def config = configLoader.loadConfig()
-		reportCreatorClassName = config.getProperty( IReportCreator.class.name )
-		outputDir = config.getProperty( ConfigLoader.PROP_OUTPUT_DIR )
-		hideEmptyBlocks = Boolean.parseBoolean(
-				config.getProperty( ConfigLoader.PROP_HIDE_EMPTY_BLOCKS )
-		)
+    private static loadSettingsFor( String prefix, Properties config ) {
+        log.fine "Loading settings for reportCreator of type $prefix"
+        Collections.list( config.propertyNames() ).grep { String key ->
+            key.startsWith prefix + '.'
+        }.collect { String key ->
+            [ ( key - ( prefix + '.' ) ): config.getProperty( key ) ]
+        }.collectEntries()
+    }
 
-		try {
-			reportCreatorSettings << loadSettingsFor( reportCreatorClassName, config )
-		} catch ( e ) {
-			log.log(Level.FINE, "Error configuring ${this.class.name}!", e)
-		}
-	}
+    private void configReportCreator( IReportCreator reportCreator ) {
+        reportCreator.outputDir = outputDir
+        reportCreator.hideEmptyBlocks = hideEmptyBlocks
+        def reportCreatorSettings = [:]
+        try {
+            reportCreatorSettings << loadSettingsFor( reportCreator.class.name, config )
+        } catch ( e ) {
+            log.fine( "Error configuring ${reportCreator.class.name}!", e )
+        }
 
-	def instantiateReportCreator() {
-		def reportCreatorClass = Class.forName( reportCreatorClassName )
-		reportCreatorClass.asSubclass( IReportCreator ).newInstance()
-	}
-
-	private static loadSettingsFor( String prefix, Properties config ) {
-		Collections.list( config.propertyNames() ).grep { String key ->
-			key.startsWith prefix + '.'
-		}.collect { String key ->
-			[ ( key - ( prefix + '.' ) ): config.getProperty( key ) ]
-		}.collectEntries()
-	}
-
-	private void configReportCreator( IReportCreator reportCreator ) {
-		reportCreator.outputDir = outputDir
-		reportCreator.hideEmptyBlocks = hideEmptyBlocks
-		reportCreatorSettings.each { field, value ->
-			reportCreator."$field" = value
-		}
-	}
+        reportCreatorSettings.each { field, value ->
+            reportCreator."$field" = value
+        }
+    }
 
 }
 
 class SpecInfoListener implements IRunListener {
 
-	final IReportCreator reportCreator
-	SpecData specData
-	IterationInfo currentIteration
-	long startT
+    final IReportCreator reportCreator
+    SpecData specData
+    IterationInfo currentIteration
+    long startT
 
-	SpecInfoListener( IReportCreator reportCreator ) {
-		this.reportCreator = reportCreator
-	}
+    SpecInfoListener( IReportCreator reportCreator ) {
+        this.reportCreator = reportCreator
+    }
 
-	@Override
-	synchronized void beforeSpec( SpecInfo spec ) {
-		specData = new SpecData( info: spec )
-		startT = System.currentTimeMillis()
-	}
+    @Override
+    synchronized void beforeSpec( SpecInfo spec ) {
+        specData = new SpecData( info: spec )
+        startT = System.currentTimeMillis()
+    }
 
-	@Override
-	void beforeFeature( FeatureInfo feature ) {
-		specData.featureRuns << new FeatureRun( feature: feature )
-	}
+    @Override
+    void beforeFeature( FeatureInfo feature ) {
+        specData.featureRuns << new FeatureRun( feature: feature )
+    }
 
-	@Override
-	void beforeIteration( IterationInfo iteration ) {
-		currentRun().failuresByIteration[ iteration ] = [ ]
-		currentIteration = iteration
-	}
+    @Override
+    void beforeIteration( IterationInfo iteration ) {
+        currentRun().failuresByIteration[ iteration ] = [ ]
+        currentIteration = iteration
+    }
 
-	@Override
-	void afterIteration( IterationInfo iteration ) {
-		currentIteration = null
-	}
+    @Override
+    void afterIteration( IterationInfo iteration ) {
+        currentIteration = null
+    }
 
-	@Override
-	void afterFeature( FeatureInfo feature ) {
+    @Override
+    void afterFeature( FeatureInfo feature ) {
 
-	}
+    }
 
-	@Override
-	void afterSpec( SpecInfo spec ) {
-		assert specData.info == spec
-		specData.totalTime = System.currentTimeMillis() - startT
-		reportCreator.createReportFor specData
-		specData = null
-	}
+    @Override
+    void afterSpec( SpecInfo spec ) {
+        assert specData.info == spec
+        specData.totalTime = System.currentTimeMillis() - startT
+        reportCreator.createReportFor specData
+        specData = null
+    }
 
-	@Override
-	void error( ErrorInfo error ) {
+    @Override
+    void error( ErrorInfo error ) {
         def iteration = currentIteration ?: dummySpecIteration()
         currentRun().failuresByIteration[ iteration ] << new SpecProblem( error )
-	}
+    }
 
-	@Override
-	void specSkipped( SpecInfo spec ) {
-		// specInfo already knows if it's skipped
-	}
+    @Override
+    void specSkipped( SpecInfo spec ) {
+        // specInfo already knows if it's skipped
+    }
 
-	@Override
-	void featureSkipped( FeatureInfo feature ) {
-		// feature already knows if it's skipped
-	}
+    @Override
+    void featureSkipped( FeatureInfo feature ) {
+        // feature already knows if it's skipped
+    }
 
     private FeatureRun currentRun() {
         if ( specData.featureRuns.empty ) {
--- src/test/groovy/com/athaydes/spockframework/report/SpockReportExtensionSpec.groovy
+++ src/test/groovy/com/athaydes/spockframework/report/SpockReportExtensionSpec.groovy
@@ -10,68 +10,70 @@ import spock.lang.Specification
  */
 class SpockReportExtensionSpec extends Specification {
 
-	def "The settings found in the config.properties file are read only once"( ) {
-		given:
-		"An instance of SpockReportExtension with a mocked out config loader"
-		def extension = new SpockReportExtension()
-		extension.configLoader = Mock ConfigLoader
+    def "The settings found in the config.properties file are read only once"() {
+        given:
+        "An instance of SpockReportExtension with a mocked out config loader"
+        def extension = new SpockReportExtension()
+        extension.configLoader = Mock ConfigLoader
 //        extension.configLoader.loadConfig() >> new Properties()
 
-		when:
-		"Spock visits 10 spec"
+        when:
+        "Spock visits 10 spec"
         10.times { extension.visitSpec( Mock( SpecInfo ) ) }
 
-		then:
-		"The config is read once"
-		1 * extension.configLoader.loadConfig() >> new Properties()
-	}
+        then:
+        "The config is read once"
+        1 * extension.configLoader.loadConfig() >> new Properties()
+    }
 
-	def "The settings found in the config.properties file are used to configure the report framework"( ) {
-		given:
-		"A mock ReportCreator"
-		def mockReportCreator = GroovyMock( IReportCreator )
+    def "The settings found in the config.properties file are used to configure the report framework"() {
+        given:
+        "A mock ReportCreator"
+        def mockReportCreator = GroovyMock( IReportCreator )
+        def className = 'MockReportCreator'
+        mockReportCreator.getClass() >> [ name: className ]
 
-		and:
-		"A mock ConfigLoader"
-		def mockConfigLoader = Mock( ConfigLoader )
-		def props = new Properties()
-		mockConfigLoader.loadConfig() >> props
+        and:
+        "A mock ConfigLoader"
+        def mockConfigLoader = Mock( ConfigLoader )
+        def props = new Properties()
+        mockConfigLoader.loadConfig() >> props
 
-		and:
-		"A mock Spec"
-		def mockSpecInfo = Mock( SpecInfo )
+        and:
+        "A mock Spec"
+        def mockSpecInfo = Mock( SpecInfo )
 
-		and:
-		"An instance of SpockReportExtension with mocked out config loader and reportCreator"
-		def extension = new SpockReportExtension() {
-			@Override
-			def instantiateReportCreator( ) { mockReportCreator }
-		}
-		extension.configLoader = mockConfigLoader
+        and:
+        "An instance of SpockReportExtension with mocked out config loader and reportCreator"
+        def extension = new SpockReportExtension() {
+            @Override
+            def instantiateReportCreator() { mockReportCreator }
+        }
+        extension.configLoader = mockConfigLoader
 
-		and:
-		"A set of valid and invalid properties emulating the properties file"
-		props.setProperty( IReportCreator.class.name, "MockReportCreator" )
-		props.setProperty( "MockReportCreator" + '.customProp', 'customValue' )
-		props.setProperty( "com.athaydes.spockframework.report.outputDir", "the-output-dir" )
-		props.setProperty( "some.invalid.property", "invalid-value" )
+        and:
+        "A set of valid and invalid properties emulating the properties file"
+        props.setProperty( IReportCreator.class.name, className )
+        props.setProperty( className + '.customProp', 'customValue' )
+        props.setProperty( "com.athaydes.spockframework.report.outputDir", "the-output-dir" )
+        props.setProperty( "some.invalid.property", "invalid-value" )
 
-		when:
-		"This extension framework is initiated by Spock visiting the mock spec"
-		extension.visitSpec( mockSpecInfo )
+        when:
+        "This extension framework is initiated by Spock visiting the mock spec"
+        extension.visitSpec( mockSpecInfo )
 
-		then:
-		"The class of the implementation of ReportCreator was correctly set"
-		extension.reportCreatorClassName == "MockReportCreator"
+        then:
+        "The class of the implementation of ReportCreator was correctly set"
+        extension.reportCreatorClassName == "MockReportCreator"
 
-		and:
-		"This extension added a SpecInfoListener to Spock's SpecInfo"
-		1 * mockSpecInfo.addListener( _ )
+        and:
+        "This extension added a SpecInfoListener to Spock's SpecInfo"
+        1 * mockSpecInfo.addListener( _ )
 
-		and:
-		"The ReportCreator was configured with the valid properties"
-		1 * mockReportCreator.setOutputDir( "the-output-dir" )
-		1 * mockReportCreator.setCustomProp( "customValue" )
-	}
+        and:
+        "The ReportCreator was configured with the valid properties"
+        1 * mockReportCreator.setOutputDir( "the-output-dir" )
+        1 * mockReportCreator.setCustomProp( "customValue" )
+    }
 
 }
